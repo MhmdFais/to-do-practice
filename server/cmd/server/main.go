a22f228621f4ff2cc14276c9baf64d26a4d84257
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2026-02-25 07:00:19 +0530

    further concepts

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	secondaryFunction()
+	fmt.Println()
+	var result int = addTwoNumbers(5, 10)  // result := addTwoNumbers(5, 10)
+	fmt.Printf("The result of adding 5 and 10 is: %d\n", result)
+	fmt.Println()
+	age := 20
+	fmt.Println(ifStatementExample(age))
+}
+
+func secondaryFunction() {
+	fmt.Println("This is a secondary function.")
+}
+
+func addTwoNumbers(numberOne int, numberTwo int) int {
+	return numberOne + numberTwo
+}
+
+func ifStatementExample(age int) string {
+	if age >= 18 {
+		return "You are an adult."
+	} else {
+		return "You are a minor."
+	}
 }
