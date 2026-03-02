08551caf1c9a470a9f1fade8e33ab646438fd8e2
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2026-03-02 13:49:20 +0530

    more concepts

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -10,6 +10,10 @@ func main() {
 	fmt.Println()
 	age := 20
 	fmt.Println(ifStatementExample(age))
+	fmt.Println()
+	arrayExample()
+	fmt.Println()
+	sliceExample()
 }
 
 func secondaryFunction() {
@@ -26,4 +30,27 @@ func ifStatementExample(age int) string {
 	} else {
 		return "You are a minor."
 	}
+}
+
+func arrayExample(){
+	var numbers[5]int
+
+	// numbes := [5]int{10, 20, 30, 40, 50} // Array literal
+
+	numbers[0] = 10
+	numbers[1] = 20
+	numbers[2] = 30
+	numbers[3] = 40
+	numbers[4] = 50
+
+	fmt.Println("Array of integers:", numbers)
+	fmt.Println(numbers[0:5])
+}
+
+func sliceExample() {
+	slice := []int{10, 20, 30, 40, 50}
+	fmt.Println("Slice of integers:", slice)
+	
+	slice = append(slice, 60)
+	fmt.Println("Slice after appending 60:", slice)
 }
